88e10d5226d50db6fb1d1e65e087570e35133b0a
nininks03 <nininks03@gmail.com>
2025-03-21 16:19:08 +0530

    first commit

---
+++ login.php
@@ -0,0 +1,89 @@
+<?php
+    session_start();
+    if(isset($_SESSION['username'])){
+        header("Location: welcome.php");
+    }
+?>
+<?php
+    $login = false;
+    include('connection.php');
+    if (isset($_POST['submit'])) {
+        $username = $_POST['user'];
+        $password = $_POST['pass'];
+        echo $password;
+        $sql = "select * from users where username = '$username'or email = '$username'";  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+        
+        if($row){  
+            echo $count;
+
+            if(password_verify($password, $row["password"])){
+                $login=true;
+                session_start();
+
+                $sql = "select username from users where username = '$username'or email = '$username'";     
+                $r = mysqli_fetch_array(mysqli_query($conn, $sql), MYSQLI_ASSOC);  
+
+                $_SESSION['username']= $r['username'];
+                $_SESSION['loggedin'] = true;
+                header("Location: welcome.php");
+            }
+        }  
+        else{  
+            echo  '<script>
+                        
+                        alert("Login failed. Invalid username or password!!")
+                        window.location.href = "login.php";
+                    </script>';
+        }     
+    }
+    ?>
+    <?php 
+    include("connection.php");
+    ?>
+    
+<html>
+    <head>
+        <title>Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    </head>
+    <body>
+        <h1>LOGIN TO NEWSILO</h1>
+        <br><br>
+        <div id="form">
+            <form name="form" action="login.php" method="POST" required>
+                <label>Enter Username/Email: </label>
+                <input type="text" id="user" name="user"></br></br>
+                <label>Password: </label>
+                <input type="password" id="pass" name="pass" required></br></br>
+                <input type="submit" id="btn" value="Login" name = "submit"/>
+                <p class="dot"><b>----------or----------</b></p>
+                <div class="icons">
+                    <i class="fab fa-google"></i>
+                    <i class="fab fa-facebook"></i>
+                    <i class="fab fa-instagram"></i>
+                </div>
+                <div class="links">
+                    <p>Don't have an account yet?</p>
+                    <a href="signup.php" id="signUpButton" class="btnSign">Sign Up</a>
+                </div>
+            </form>
+        </div>
+        <script>
+            function isvalid(){
+                var user = document.form.user.value;
+                if(user.length==""){
+                    alert(" Enter username or email id!");
+                    return false;
+                }
+                
+            }
+        </script>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid"> 
+          <img src="images\LOGO.png" alt="..." style="height: 50px;"><div style="font-weight: 700;font-size:larger">NEWSILO</div>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left: 720px;">          
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-DARK" type="submit">Search</button>
+            </form>
+            &nbsp;
+            <form class="d-flex" >
+              <a class="btn btn-dark" type="submit" href="logout.php">Logout</a>
+            </form>
+          </div>
+        </div>
+    </nav>
+
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">HOME</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">ENTERTAINMENT</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">TECHNOLOGY</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">POLITICS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">EDUCATION</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">SPORTS</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">TRAVEL</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+        </div>
+      </nav>
